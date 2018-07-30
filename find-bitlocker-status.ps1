8e939ffddc1c53da730661ea9b8eb3060dc6d613
Luke Verlooy <verlooy@gmail.com>
2018-07-30 00:40:23 -0500

    changed everything

--- find-bitlocker-status.ps1
+++ find-bitlocker-status.ps1
@@ -2,63 +2,39 @@
 # A new partition or if bitlocker is configured properly. Changes will have to be made to create a bitlocker partition
 # Error code for success: '3231711234' but converts --> '-1063256062'
 # Make sure you have permissions to run the script (Enable-PSRemoting -Force)
-# Add your host or ips in $comp_list and make sure you have a folder for output
+# Add your host or ips in the computer list and make sure you have a folder for output
 
 
-# Read file line by line
-$comp_list = Get-Content "C:\ps-scripts\computernames.txt"
+$err=@()
 
-$myScript = {
+$computerlist = Get-Content "computernames.txt"
 
-	#needed for -ArgumentList to work
-	param($online)
-
-	$computers = $online
-
-	foreach($computer in $computers){
-		$Result = bdehdcfg -driveinfo
-
-		if($LASTEXITCODE -eq -1063256062){
-			Write-Host -ForegroundColor Green $computer ": Is properly Configured"
-			write-output $computer >> c:\ps-scripts\successs.txt
-
-
-}
-
-		else{
-
-			Write-Host -ForegroundColor Red $computer ": Is not configured"
-}
-
-
-}
-
-}
-
-
-
-# iterate through the list
-foreach($item in $comp_list) {
-
-# test the connection
-	if(Test-Connection -ComputerName $item -Buffersize 16 -Count 1 -Quiet){
-
-		$online = $item
-
-		Invoke-Command -ComputerName $online -ArgumentList $online -ScriptBlock $myScript
-}
+$myScript={
+	param($computername)
+	$Result = BdeHdCfg.exe -driveinfo
+	$theErrors = $computername + $Result
+	if($LASTEXITCODE -eq -1063256062){
+		Write-Host -ForegroundColor Green $computername ": Is properly Configured"
+		write-output $computername >> C:\somepath\success.txt
+	}
 
 	else{
 
-		$badhost = $item
-		Write-Host -ForegroundColor Red $badhost ": is offline or icmp is blocked"
+		Write-Host -ForegroundColor Red $computername `
+		"Exit code is different or not properly configured, check logs"
+	}
 
+	Write-Output $err | Out-File -Append -FilePath C:\somepath\error.txt
+	Write-Output $theErrors | Out-File -Append -FilePath C:\somepath\bde-log.txt
 }
 
 
 
+foreach($computername in $computerlist){
+	Invoke-Command -ComputerName $computername `
+	-ArgumentList $computername -ScriptBlock $myScript `
+	-EA SilentlyContinue -ErrorVariable +err
+
 }
-
-
-
+    
 
